737c99fd62a394ca4fdb95a9ff5559a2d3981846
Nuñez <mikhail.nunez@accenture.com>
2022-09-05 16:15:49 +0800

    add player sprites; adjust screen size

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const canvas = document.querySelector('canvas');
 const c = canvas.getContext('2d');
-canvas.width = innerWidth;
-canvas.height = innerHeight;
+canvas.width = 800;
+canvas.height = 500;
 
 class Sprite {
     constructor({ image, position, scale = 1, speed = 1 }) {
@@ -48,17 +48,35 @@ const velocity = {
     dy: 0
 }
 
+let playerSpriteIndex = 0;
+function rotatePlayerSpriteIndex() {
+    if (++playerSpriteIndex > 2) {
+        playerSpriteIndex = 0;
+    }
+    console.log(playerSpriteIndex);
+    return playerSpriteIndex;
+}
+const playerImages = {
+    UP: [document.getElementById('player_up_1'), document.getElementById('player_up_2'), document.getElementById('player_up_3')],
+    DOWN: [document.getElementById('player_down_1'), document.getElementById('player_down_2'), document.getElementById('player_down_3')],
+    LEFT: [document.getElementById('player_left_1'), document.getElementById('player_left_2'), document.getElementById('player_left_3')],
+    RIGHT: [document.getElementById('player_right_1'), document.getElementById('player_right_2'), document.getElementById('player_right_3')],
+}
+
 const map1 = document.getElementById("map_1");
 const mapSprite = new Sprite({
     image: map1,
-    position: {x: 0, y: -300},
-    scale: 1.5
+    position: {x: -80, y: -1650},
+    scale: 1.25
 });
 
 const playerUp1 = document.getElementById("player_up_1");
 const playerSprite = new Sprite({
-    image: playerUp1,
-    position: {x: canvas.width / 2, y: canvas.height / 2}
+    image: playerImages.UP[playerSpriteIndex],
+    position: {
+        x: canvas.width / 2 - playerUp1.width / 2, 
+        y: canvas.height / 2 - playerUp1.height / 2
+    }
 });
 
 function animate() {
@@ -75,25 +93,29 @@ window.addEventListener('keydown', function(e) {
         case 'W':
         case 'ArrowUp':
             velocity.dx = 0;
-            velocity.dy = speed;       
+            velocity.dy = speed;
+            playerSprite.image = playerImages.UP[rotatePlayerSpriteIndex()];       
             break;  
         case 'a':
         case 'A':
         case 'ArrowLeft':
             velocity.dx = speed;
             velocity.dy = 0;
+            playerSprite.image = playerImages.LEFT[rotatePlayerSpriteIndex()]; 
             break;    
         case 's':
         case 'S':
         case 'ArrowDown':
             velocity.dx = 0;
-            velocity.dy = -speed;      
+            velocity.dy = -speed;  
+            playerSprite.image = playerImages.DOWN[rotatePlayerSpriteIndex()];     
             break;
         case 'd':
         case 'D':
         case 'ArrowRight':
             velocity.dx = -speed;
-            velocity.dy = 0;       
+            velocity.dy = 0; 
+            playerSprite.image = playerImages.RIGHT[rotatePlayerSpriteIndex()];       
             break;          
     }
 });
